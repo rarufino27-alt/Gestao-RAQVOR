4b8080f1554c3eda4ed18eb78ac1866628e38d35
rarufino27-alt <rarufino27@gmail.com>
2026-08-22 23:43:35 -0300

    Delete desktop directory

--- desktop/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='raqvor-v2-13';
-const CORE=['./','./index.html','./styles.css','./app.js?v=2.13.0','./manifest.webmanifest','./assets/raquor-logo.png','./assets/icon-192x192.png','./assets/icon-512x512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
